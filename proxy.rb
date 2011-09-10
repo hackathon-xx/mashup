a91a2f21d35ed774b0e201453ed1b730bbc9a3c0
Marcin Bunsch <marcin@futuresimple.com>
2011-09-10 13:14:56 +0200

    Small refactor

--- proxy.rb
+++ proxy.rb
@@ -4,45 +4,9 @@ require "sinatra"
 require 'net/http'
 require 'net/https'
 require 'cgi'
+require "lib/proxies/pipejump"
 
 get "/" do
   erb :home
 end
 
-get "/api/sales/*" do |url|
-  url = url.sub("/api/sales/", "")
-  url = URI.parse("https://sales.futuresimple.com/api/v1/#{url}")
-  puts "Looking up #{url}"
-  headers = {}
-  # puts "=" * 100
-  # puts request['X-Pipejump-Auth']
-  # puts request.inspect
-  headers['X-Pipejump-Auth'] = params["token"] # request['X-Pipejump-Auth'].to_s
-  req = Net::HTTP::Get.new(url.path, headers)
-  http = Net::HTTP.new(url.host, url.port)
-  http.use_ssl = true
-  res = http.request(req)
-  status res.code
-  res.body
-end
-
-post "/api/sales/*" do |url|
-  url = url.sub("/api/sales/", "")
-  url = URI.parse("https://sales.futuresimple.com/api/v1/#{url}")
-  puts "Looking up #{url}"
-  headers = {}
-  headers['X-Pipejump-Auth'] = request['X-Pipejump-Auth'].to_s
-  req = Net::HTTP::Post.new(url.path, headers)
-  req.set_form_data(request.POST)
-  http = Net::HTTP.new(url.host, url.port)
-  http.use_ssl = true
-  res = http.request(req)
-  status res.code
-  res.body
-end
-
-put "/api/sales/*" do |url|
-end
-
-delete "/api/sales/*" do |url|
-end
--- public/scripts/pipejump.js
+++ public/scripts/pipejump.js
@@ -1,19 +1,21 @@
 Pipejump = {
   token: null,
+  logout: function() {
+    $.cookie('pipejump_token', null)
+  },
   login: function(username, password, success, failure) {
     $.post('/api/sales/authentication.json', 'email='+username+'&password='+password, function(data) {
       Pipejump.token = data.authentication.token
+      $.cookie('pipejump_token', Pipejump.token)
     }, 'json').success(success).error(failure)
   },
   getContacts: function(options, success) {
     if (!options) options = { page: 1 }
-
     $.ajax({
       dataType: 'json',
       url: '/api/sales/contacts.json?page=1&token=' + Pipejump.token,
       beforeSend: function(jqXHR) {
-        // console.log
-        // jqXHR.setRequestHeader("X-Pipejump-Auth", Pipejump.token);
+        jqXHR.setRequestHeader('X-Pipejump-Auth', Pipejump.token)
       }
     }).success(success)
   }
@@ -50,7 +52,6 @@ var Template = {
 var displayContactList = function() {
   $('#container').html('Loading...')
   Pipejump.getContacts({}, function(contacts) {
-    // console.log(contacts)
     $('#container').render('contacts', { contacts: contacts })
   })
 };
@@ -61,7 +62,6 @@ var displayLoginBox = function() {
   $('.login').submit(function() {
     var params = $(this).extractParams()
     Pipejump.login(params.email, params.password, function() {
-      $.cookie('pipejump_token', Pipejump.token)
       displayContactList()
     }, function() {
       alert('wrong!')
