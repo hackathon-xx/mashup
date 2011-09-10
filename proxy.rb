29b651293af75116ff651afc7bed80bc61c263a1
Marcin Bunsch <marcin@futuresimple.com>
2011-09-10 20:21:18 +0200

    More corrections

--- proxy.rb
+++ proxy.rb
@@ -4,8 +4,7 @@ require "sinatra"
 require 'net/http'
 require 'net/https'
 require 'cgi'
-require "lib/proxies/pipejump"
-require "oauth"
+require File.expand_path('lib/proxies/pipejump', File.dirname(__FILE__))
 
 get "/" do
   erb :home
